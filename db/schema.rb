0718465edc8e9cdd5bbedd0b31f984afb2176f0d
sandro santos <sandroucb@gmail.com>
2015-07-26 19:47:47 -0400

    Create pics scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150705050030) do
+ActiveRecord::Schema.define(version: 20150726131522) do
+
+  create_table "pics", force: :cascade do |t|
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "pins", force: :cascade do |t|
     t.string   "description"
-    t.datetime "created_at",  
-    t.datetime "updated_at",  
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -30,8 +36,8 @@ ActiveRecord::Schema.define(version: 20150705050030) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                         
-    t.datetime "updated_at",                          
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
