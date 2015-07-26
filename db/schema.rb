44f03ac080ec0b4ddefd35617ad7d1376bf42ff2
sandro santos <sandroucb@gmail.com>
2015-07-26 08:20:49 -0400

    Update Devise views

--- db/schema.rb
+++ db/schema.rb
@@ -15,8 +15,8 @@ ActiveRecord::Schema.define(version: 20150705050030) do
 
   create_table "pins", force: :cascade do |t|
     t.string   "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",  
+    t.datetime "updated_at",  
   end
 
   create_table "users", force: :cascade do |t|
@@ -30,8 +30,8 @@ ActiveRecord::Schema.define(version: 20150705050030) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                         
+    t.datetime "updated_at",                          
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
